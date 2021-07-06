30af161e5639935bca833eafd66ea3ae1ecca98f
Tommy Gatti <tommy.gatti@outlook.com>
2021-07-06 22:07:10 +1000

    added boilerplate

--- ChefsHand WatchKit Extension/InterfaceController.swift
+++ ChefsHand WatchKit Extension/InterfaceController.swift
@@ -7,12 +7,18 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var label: WKInterfaceLabel!
+    let session = WCSession.default
+    
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
+        session.delegate = self
+        session.activate()
     }
     
     override func willActivate() {
@@ -22,5 +28,22 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
     }
+    
+    @IBAction func tapSendDataToiPhone() {
+        let data: [String: Any] = ["watch": "This is from my watch!" as String]
+        session.sendMessage(data, replyHandler: nil, errorHandler: nil)
+    }
+}
 
+extension InterfaceController: WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String: Any]) {
+        print("got message from iphone! \(message)")
+        if let value = message["iPhone"] as? String {
+            self.label.setText(value)
+        }
+    }
 }
--- ChefsHand/ViewController.swift
+++ ChefsHand/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import WatchConnectivity
 
 class ViewController: UIViewController {
-
+    var session: WCSession?
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var urlField: UITextField!
+    
+    @IBAction func tapSendDataToWatch(_ sender: Any){
+        if let validSession = self.session, validSession.isReachable {
+            let data: [String: Any] = ["iPhone": "data from phone!" as Any]
+            validSession.sendMessage(data, replyHandler: nil, errorHandler: nil)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.configureWatchKitSession()
     }
-
-
+    
+    func configureWatchKitSession() {
+        if WCSession.isSupported() {
+            session = WCSession.default
+            session?.delegate = self
+            session?.activate()
+        }
+    }
 }
+
+extension ViewController: WCSessionDelegate {
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        print("Session between watch and iPhone was deactivated")
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String: Any]){
+        print("got message: \(message)")
+        DispatchQueue.main.async {
+            if let value = message["watch"] as? String {
+                self.label.text = value
+            }
+        }
+    }
+}
+
 
