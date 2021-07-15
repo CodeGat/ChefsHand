1b2ebf42d83828461f92397d4f2068538783332b
Tommy Gatti <tommy.gatti@outlook.com>
2021-07-15 22:00:35 +1000

    Stub timer added for first cooking timer, if it exists

--- ChefsHand WatchKit Extension/Method/MethodController.swift
+++ ChefsHand WatchKit Extension/Method/MethodController.swift
@@ -9,5 +9,27 @@ import Foundation
 import WatchKit
 
 class MethodController: WKInterfaceController {
+    @IBOutlet weak var methodTable: WKInterfaceTable!
     
+    override func table(_ table: WKInterfaceTable, didSelectRowAt rowIndex: Int) {
+        
+    }
+    
+    override func willActivate() {
+        let steps: [Recipe.StructuredRecipe.Step] = Recipe.shared.getRecipe().method
+        methodTable.setNumberOfRows(steps.count, withRowType: "Method Row")
+        
+        for (index, step) in steps.enumerated() {
+            let rowController  = methodTable.rowController(at: index) as! MethodRowController
+            let recipe: Recipe.StructuredRecipe = Recipe.shared.getRecipe()
+            let recipeRow: Recipe.StructuredRecipe.Step = recipe.method[index]
+            
+            rowController.methodLabel.setText(step.instruction)
+            
+            if (recipeRow.cookingTimes.count > 0){
+                rowController.methodTimer.setDate(NSDate(timeIntervalSinceNow: TimeInterval(recipeRow.cookingTimes[0].time)) as Date)
+            }
+            
+        }
+    }
 }
---
+++ ChefsHand WatchKit Extension/Method/MethodRowController.swift
@@ -0,0 +1,28 @@
+//
+//  MethodRowController.swift
+//  ChefsHand WatchKit Extension
+//
+//  Created by Tommy Gatti on 15/7/21.
+//
+
+import WatchKit
+
+class MethodRowController: NSObject {
+    @IBOutlet weak var methodLabel: WKInterfaceLabel!
+    @IBOutlet weak var separator: WKInterfaceSeparator!
+    @IBOutlet weak var methodTimer: WKInterfaceTimer!
+    
+    var step: Recipe.StructuredRecipe.Step? {
+        didSet {
+            guard let step = step else {return}
+            self.methodLabel.setText(step.instruction)
+        }
+    }
+    
+    var cookingTimer: Recipe.StructuredRecipe.CookingTimer? {
+        didSet {
+            guard let cookingTimer = cookingTimer else {return}
+            self.methodTimer.setDate(NSDate(timeIntervalSinceNow: TimeInterval(cookingTimer.time)) as Date)
+        }
+    }
+}
